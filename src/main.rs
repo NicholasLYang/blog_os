a796bbec2e804ee7ece43bdae8c6f7c83c156897
Nicholas Yang <ny585@nyu.edu>
2019-12-12 15:32:25 -0500

    Working on test chapter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -14,7 +50,10 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    panic!("Some panic message");
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     loop {}
